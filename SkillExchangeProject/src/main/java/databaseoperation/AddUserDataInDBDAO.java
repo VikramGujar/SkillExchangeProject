e98cb70301acf97a45c5588f2022920326a9c58f
Gokul <gokullade89@gmail.com>
2024-12-09 21:28:54 +0530

    Database code to store user data in oracle database

---
+++ SkillExchangeProject/src/main/java/databaseoperation/AddUserDataInDBDAO.java
@@ -0,0 +1,36 @@
+package databaseoperation;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javabean.UserDataBean;
+
+public class AddUserDataInDBDAO {
+	
+	int k=0;
+	
+	public int addData(UserDataBean ub)
+	{
+		Connection con=DatabaseConnection.getConnection();
+		try(con)
+		{
+			PreparedStatement ps=con.prepareStatement("Insert into skillExchangeusers values(?,?,?,?)");
+			
+			ps.setString(1, ub.getFname());
+			ps.setString(2, ub.getLname());
+			ps.setString(3, ub.getUsername());
+			ps.setString(4, ub.getPass());
+			
+			k=ps.executeUpdate();
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		return k;
+		
+	}
+
+}
--- SkillExchangeProject/src/main/java/userinput/RegisterServlet.java
+++ SkillExchangeProject/src/main/java/userinput/RegisterServlet.java
@@ -1,5 +1,50 @@
 package userinput;
 
-public class RegisterServlet {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import databaseoperation.AddUserDataInDBDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import javabean.UserDataBean;
+
+@SuppressWarnings("serial")
+
+@WebServlet("/reg")
+public class RegisterServlet extends HttpServlet {
+
+	UserDataBean ub=null;
+	AddUserDataInDBDAO au=null;
+	@Override
+	public void init()
+	{
+		ub= new UserDataBean();
+		au= new AddUserDataInDBDAO();
+	}
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
+	{
+		ub.setFname(req.getParameter("first-name"));
+		ub.setLname(req.getParameter("last-name"));
+		ub.setUsername(req.getParameter("username"));
+		ub.setPass(req.getParameter("password"));
+		
+		int result=au.addData(ub);
+		
+		if(result>0)
+		{
+			
+		}
+		else
+		{
+			res.setContentType("text/html");
+			
+			PrintWriter pw=res.getWriter();
+			pw.print("Something is wrong try again");
+			req.getRequestDispatcher("registration.html").include(req, res);
+		}
+	}
 }
