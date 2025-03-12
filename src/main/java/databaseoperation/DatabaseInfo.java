b1ce374217e32b5b874e2b8660b50c899b7f7abc
ChanchalSethi <chanchalsethi1904@gmail.com>
2025-03-13 00:08:56 +0530

    commit

--- src/main/java/databaseoperation/DatabaseInfo.java
+++ src/main/java/databaseoperation/DatabaseInfo.java
@@ -6,7 +6,7 @@ public interface DatabaseInfo {
 	//Database information
 	public static final String driver="oracle.jdbc.driver.OracleDriver";
 	public static final String dbUrl="jdbc:oracle:thin:@localhost:1521:orcl";
-	public static final String dbUName="Gokul";
-	public static final String dbPassword="9112";
+	public static final String dbUName="system";
+	public static final String dbPassword="lion";
 	
 }
