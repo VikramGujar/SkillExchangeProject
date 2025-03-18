0fd8dd0d284a639d1bf6117ab7949135654cdef1
Gokul <gokullade89@gmail.com>
2025-03-18 07:49:36 +0530

    Database Info Change

--- src/main/java/databaseoperation/DatabaseInfo.java
+++ src/main/java/databaseoperation/DatabaseInfo.java
@@ -5,12 +5,7 @@ public interface DatabaseInfo {
 
 	//Database information
 	public static final String driver="oracle.jdbc.driver.OracleDriver";
-	public static final String dbUrl="jdbc:oracle:thin:@localhost:1521:xe";
+	public static final String dbUrl="jdbc:oracle:thin:@localhost:1521:orcl";
 	public static final String dbUName="system";
-<<<<<<< HEAD
-	public static final String dbPassword="tonystark";
-=======
 	public static final String dbPassword="tiger";
->>>>>>> 7123851c8b7e7aa60137756e94a155f4efa28b11
-	
 }
